b55c87c428e80d8950e1f04b565dd6625f73e454
DaimyoDev <85042261+DaimyoDev@users.noreply.github.com>
2022-04-17 15:38:36 -0500

    initialstuff

---
+++ starwarsmovies/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="bg-slate-600 w-screen h-screen box-border">
+      <div className="w-screen h-35 bg-zinc-500 flex align-text-center shadow-2xl">
+        <h1 className="text-orange-500 font-bold text-4xl text-center drop-shadow-lg m-auto p-5">
+          Star Wars Movies
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
